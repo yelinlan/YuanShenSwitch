3108854a8faba27158d97d62b3983d20670f02be
夜林蓝 <1562098029@qq.com>
2023-09-23 14:36:42 +0800

    1.添加修改安装目录下的config.ini. 避免reigion not match 问题。 2.关于密码记录问题。可以参见这位up主   https://www.bilibili.com/read/cv11811683/

--- src/main/java/tools/SwitchYuanShenClient.java
+++ src/main/java/tools/SwitchYuanShenClient.java
@@ -1,13 +1,10 @@
 package tools;
 
 import cn.hutool.core.io.FileUtil;
-import cn.hutool.core.io.resource.ClassPathResource;
 import cn.hutool.setting.Setting;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.Scanner;
 
 /**
@@ -19,18 +16,35 @@ import java.util.Scanner;
  **/
 public class SwitchYuanShenClient {
 
-	//基地址
-	public static final String BASE_PATH = "C:\\Program Files\\Genshin Impact\\Genshin Impact Game";
-	//配置文件与插件
-	public static final String CONFIG_PATH = BASE_PATH + "\\config.ini";
-	public static final String BILIBILI_PLUGIN = BASE_PATH + "\\YuanShen_Data\\Plugins\\PCGameSDK.dll";
-
-	//备份基地址
+	/**
+	 * 初始化配置 安装目录 与 备份目录
+	 */
+	//安装路径
+	// TODO: 2023/9/23 可以修改这里两个目录
+	public static final String INSTALL_PATH = "C:\\Program Files\\Genshin Impact";
+	//备份CONFIG
 	public static final String BACK_UP_BASE = "D:\\YuanShen";
+
+	/**
+	 * GAME目录
+	 */
+	public static final String GAME_PATH = INSTALL_PATH + "\\Genshin Impact Game";
+	//配置文件与插件
+	public static final String GAME_CONFIG_PATH = GAME_PATH + "\\config.ini";
+	// TODO: 2023/9/23 自己下载的插件放这里
+	public static final String GAME_BILIBILI_PLUGIN = GAME_PATH + "\\YuanShen_Data\\Plugins\\PCGameSDK.dll";
 	//备份
-	public static final String BACK_UP_CONFIG_PATH_INIT = BACK_UP_BASE + "\\config.ini.bak";
+	public static final String GAME_BACK_UP_CONFIG_PATH_INIT = BACK_UP_BASE + "\\GAME\\config.ini.bak";
 	//如果是B服会有这个插件。如果是官服就没有，需要手动放进来。
-	public static final String BACK_UP_BILIBILI_PLUGIN_INIT = BACK_UP_BASE + "\\PCGameSDK.dll.bak";
+	public static final String GAME_BACK_UP_BILIBILI_PLUGIN_INIT = BACK_UP_BASE + "\\GAME\\PCGameSDK.dll.bak";
+
+	/**
+	 * 安装目录
+	 */
+	//配置文件与插件
+	public static final String GLOBAL_CONFIG_PATH = INSTALL_PATH + "\\config.ini";
+	//备份
+	public static final String GLOBAL_BACK_UP_CONFIG_PATH_INIT = BACK_UP_BASE + "\\GLOBAL\\config.ini.bak";
 
 	/**
 	 * run
@@ -70,19 +84,23 @@ public class SwitchYuanShenClient {
 					switch2TianKongDao();
 					break;
 				case 3:
-					System.out.println("备份的config在这里：" + BACK_UP_CONFIG_PATH_INIT);
+					System.out.println("备份的config在这里：" + GAME_BACK_UP_CONFIG_PATH_INIT);
 					break;
 				case 4:
-					System.out.println("PCGameSDK.dll在这里：" + BACK_UP_BILIBILI_PLUGIN_INIT);
+					System.out.println("PCGameSDK.dll在这里：" + GAME_BACK_UP_BILIBILI_PLUGIN_INIT);
 					break;
 				case 5:
-					FileUtil.copy(BACK_UP_CONFIG_PATH_INIT, CONFIG_PATH, true);
+					//还原GAME
+					FileUtil.copy(GAME_BACK_UP_CONFIG_PATH_INIT, GAME_CONFIG_PATH, true);
+					//还原INSTALL
+					FileUtil.copy(GLOBAL_BACK_UP_CONFIG_PATH_INIT, GLOBAL_CONFIG_PATH, true);
+					//还原插件
 					if (isBiliBili()) {
 						//如果是B服  还原 PCGameSDK.dll
-						FileUtil.copy(BACK_UP_BILIBILI_PLUGIN_INIT, BILIBILI_PLUGIN, true);
+						FileUtil.copy(GAME_BACK_UP_BILIBILI_PLUGIN_INIT, GAME_BILIBILI_PLUGIN, true);
 					} else {
 						//如果是官服  删除 PCGameSDK.dll
-						FileUtil.del(BILIBILI_PLUGIN);
+						FileUtil.del(GAME_BILIBILI_PLUGIN);
 					}
 					System.out.println("恢复完成！");
 				case 0:
@@ -95,11 +113,14 @@ public class SwitchYuanShenClient {
 
 	}
 
+	/**
+	 * 校验目录存在
+	 */
 	private static boolean validate() {
 		//目录对不对
-		if (!BASE_PATH.contains("Genshin Impact Game") || !FileUtil.exist(BASE_PATH)) {
+		if (!INSTALL_PATH.contains("Genshin Impact") || !FileUtil.exist(INSTALL_PATH)) {
 			System.out.println("【警告】原神目录不正确！应该像这样：");
-			System.out.println(BASE_PATH);
+			System.out.println(INSTALL_PATH);
 			return false;
 		}
 
@@ -113,14 +134,6 @@ public class SwitchYuanShenClient {
 				System.out.println("【警告】备份目录为：");
 				System.out.println(file.getAbsolutePath());
 			}
-		}
-		//插件有没有
-		if (!FileUtil.exist(BACK_UP_BILIBILI_PLUGIN_INIT)){
-			InputStream in = SwitchYuanShenClient.class.getClassLoader().
-					getResourceAsStream("plugins/PCGameSDK.dll");
-			FileUtil.writeFromStream(in,BACK_UP_BILIBILI_PLUGIN_INIT);
-			System.out.println("没有找到【B服】PCGameSDK.dll插件");
-			System.out.println("复制内部插件【可能过时！】。");
 		}
 		return true;
 	}
@@ -136,7 +149,7 @@ public class SwitchYuanShenClient {
 	 * 客户端名字
 	 */
 	private static String clientName() {
-		Setting setting = new Setting(CONFIG_PATH);
+		Setting setting = new Setting(GAME_CONFIG_PATH);
 		return setting.getByGroup("cps", "General");
 	}
 
@@ -144,7 +157,7 @@ public class SwitchYuanShenClient {
 	 * 客户端名字
 	 */
 	private static String channnel() {
-		Setting setting = new Setting(CONFIG_PATH);
+		Setting setting = new Setting(GAME_CONFIG_PATH);
 		String channel = setting.getByGroup("channel", "General");
 		if ("1".equals(channel)) {
 			return "官服";
@@ -159,17 +172,28 @@ public class SwitchYuanShenClient {
 	 * 初次备份
 	 */
 	private static void firstBack() {
-		//备份config.js
-		FileUtil.copy(CONFIG_PATH, BACK_UP_CONFIG_PATH_INIT, false);
+		//备份GLOBLE_config.js
+		FileUtil.copy(GLOBAL_CONFIG_PATH, GLOBAL_BACK_UP_CONFIG_PATH_INIT, false);
+		//备份GAEM_config.js
+		FileUtil.copy(GAME_CONFIG_PATH, GAME_BACK_UP_CONFIG_PATH_INIT, false);
 		//如果是B服  备份BILIBILI PCGameSDK.dll
 		if (isBiliBili()) {
-			if (FileUtil.exist(BILIBILI_PLUGIN)){
-				FileUtil.copy(BILIBILI_PLUGIN, BACK_UP_BILIBILI_PLUGIN_INIT, false);
-			}else {
-				System.out.println("插件不存在！");
-				System.out.println(BILIBILI_PLUGIN);
+			if (FileUtil.exist(GAME_BILIBILI_PLUGIN)) {
+				FileUtil.copy(GAME_BILIBILI_PLUGIN, GAME_BACK_UP_BILIBILI_PLUGIN_INIT, false);
+			} else {
+				System.out.println("【警告】插件不存在！");
+				System.out.println(GAME_BILIBILI_PLUGIN);
 			}
 		}
+
+		//插件有没有
+		if (!FileUtil.exist(GAME_BACK_UP_BILIBILI_PLUGIN_INIT)) {
+			InputStream in = SwitchYuanShenClient.class.getClassLoader().getResourceAsStream("plugins/PCGameSDK.dll");
+			FileUtil.writeFromStream(in, GAME_BACK_UP_BILIBILI_PLUGIN_INIT);
+			System.out.println("【警告】没有找到【B服】PCGameSDK.dll插件");
+			System.out.println("【警告】复制内部插件【可能过时！】。");
+		}
+
 	}
 
 	/**
@@ -177,11 +201,22 @@ public class SwitchYuanShenClient {
 	 */
 	private static void switch2ShiJieShu() {
 		//复制插件
-		FileUtil.copy(BACK_UP_BILIBILI_PLUGIN_INIT, BILIBILI_PLUGIN, true);
-		Setting setting = new Setting(CONFIG_PATH);
-		setting.setByGroup("channel", "General", "14");
-		setting.setByGroup("cps", "General", "bilibili");
-		setting.store(CONFIG_PATH);
+		FileUtil.copy(GAME_BACK_UP_BILIBILI_PLUGIN_INIT, GAME_BILIBILI_PLUGIN, true);
+		//改变Game下的配置
+		changeGameConfig("14", "bilibili");
+		//改变安装目录下的配置
+		changeInstallConfig("14", "bilibili");
+	}
+
+	/**
+	 * 改变安装目录下的配置
+	 */
+	private static void changeInstallConfig(String channel, String cps) {
+		Setting setting = new Setting(GLOBAL_CONFIG_PATH);
+		setting.setByGroup("channel", "General", channel);
+		setting.setByGroup("cps", "General", cps);
+		setting.setByGroup("sub_channel", "General", channel);
+		setting.store(GLOBAL_CONFIG_PATH);
 	}
 
 	/**
@@ -189,10 +224,21 @@ public class SwitchYuanShenClient {
 	 */
 	private static void switch2TianKongDao() {
 		//移除BILIBILI插件
-		FileUtil.del(BILIBILI_PLUGIN);
-		Setting setting = new Setting(CONFIG_PATH);
-		setting.setByGroup("channel", "General", "1");
-		setting.setByGroup("cps", "General", "mihoyo");
-		setting.store(CONFIG_PATH);
+		FileUtil.del(GAME_BILIBILI_PLUGIN);
+		//改变Game下的配置
+		changeGameConfig("1", "mihoyo");
+		//改变安装目录下的配置
+		changeInstallConfig("1", "mihoyo");
 	}
+
+	/**
+	 * 改变Game下的配置
+	 */
+	private static void changeGameConfig(String channel, String cps) {
+		Setting setting = new Setting(GAME_CONFIG_PATH);
+		setting.setByGroup("channel", "General", channel);
+		setting.setByGroup("cps", "General", cps);
+		setting.store(GAME_CONFIG_PATH);
+	}
+
 }
